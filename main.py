051f292ed28e6543a8c28906ed968d47acd35380
PhilBradbury <phil.bradbury@manchester.ac.uk>
2022-09-24 22:22:45 +0100

    Updated UI to allow selection of user's subscription services, and filtering output based on this

--- main.py
+++ main.py
@@ -29,9 +29,16 @@ def get_data(theurl, theheaders, thequerystring):
     response = requests.request("GET", theurl, headers=theheaders, params=thequerystring)
     return response
 
+with st.sidebar:
+    show_netflix = st.checkbox("Netflix")
+    show_amazon = st.checkbox("Amazon Prime")
+    show_disney = st.checkbox("Disney+")
+    show_apple = st.checkbox("Apple")
+    show_now = st.checkbox("Now TV")
+
+
 # The data to use
 if filmtitle:
-    st.empty()
     data = get_data(url, headers, querystring)
     result = json.loads(data.content)
     
@@ -55,36 +62,65 @@ if filmtitle:
         if "original" in obj["posterURLs"]:
             image_url = obj["posterURLs"]["original"]
         
-        canaccess = False     
+        canaccess = False  
+        has_subscription = False
+        
+        services_data = ""
         netflix_class = ""
         amazon_class = ""
         disney_class = ""
+        apple_class = ""
+        now_class = ""
         
         
         if "gb" in obj["streamingInfo"]:
+            canaccess = True
             services = obj["streamingInfo"]["gb"]
     
-            if "netflix" in services:
-                canaccess = True
-                netflix_class = "nf"
-            elif "prime" in services:
-                canaccess = True
-                amazon_class = "ap"       
-            elif "disney" in services:
-                canaccess = True
-                disney_class = "dp"
-            else:
-                not_on_selected_platforms_data += "<div class='nonukdata'>" + obj["title"]
-                for service in services:
-                    not_on_selected_platforms_data += "<span class='pill'>" + service + "</span>";
-                not_on_selected_platforms_data += "</div>"
-        else:
-            non_uk_data += "<div class='nonukdata'>{0}<span class='pill'>{1}</span></div>".format(obj["title"], obj["year"])
+            for s in services:
+                if s == "netflix":
+                    if show_netflix:
+                        has_subscription = True
+                        netflix_class = "nf"
+                    services_data += "<span class='pill " + netflix_class + "'>Netflix</span>"
+                    
+                elif s == "prime":
+                    if show_amazon:
+                        has_subscription = True
+                        amazon_class = "ap"
+                    services_data += "<span class='pill " + amazon_class + "'>Amazon Prime</span>"
+                    
+                elif s == "disney":
+                    if show_disney:
+                        has_subscription = True
+                        disney_class = "dp"
+                    services_data += "<span class='pill " + disney_class + "'>Disney+</span>"
+                    
+                elif s == "apple":
+                    if show_apple:
+                        has_subscription = True
+                        apple_class = "app"
+                    services_data += "<span class='pill " + apple_class + "'>Apple</span>"
+                
+                elif s == "now":
+                    if show_now:
+                        has_subscription = True
+                        now_class = "now"
+                    services_data += "<span class='pill " + now_class + "'>Now TV</span>"
 
-        if canaccess:
+                else:
+                    services_data += "<span class='pill'>" + s + "</span>"
+            
+            if not has_subscription:
+                not_on_selected_platforms_data += "<div class='nonukdata'>" + title + services_data + "</div>"
+
+        else:
+            non_uk_data += "<div class='nonukdata'>{0}<span class='pill year-pill'>{1}</span></div>".format(obj["title"], obj["year"])
+
+        if canaccess & has_subscription:
             st.markdown(f"""
                         <div class="card">
-                            <div class="card-header">{title} <span class="pill">{year}</span></div>
+                            <div class="card-header">{title} <span class="pill year-pill">{year}</span></div>
                             <div class="card-body">
                                 <div class="image">
                                     <img class="poster" src={image_url}></td>
@@ -95,9 +131,7 @@ if filmtitle:
                                 </div>                            
                             </div>
                             <div class="card-footer">
-                                <span class="btn {netflix_class}">Netflix</span>
-                                <span class="btn {amazon_class}">Amazon Prime</span>
-                                <span class="btn {disney_class}">Disney+</span>
+                               &nbsp;{services_data}
                             </div>
                         </div>
                         """, unsafe_allow_html=True)
